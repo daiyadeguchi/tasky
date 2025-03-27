c9908d0ad1498ffc8aa44aef4efdc610f5c6ae54
daiyadeguchi <double.d.rugby@gmail.com>
2025-03-28 07:24:44 +0900

    add auth0 again

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.ts'
+import { createAuth0 } from '@auth0/auth0-vue'
 
 createApp(App)
   .use(router)
+  .use(createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENTID,
+    authorizationParams: {
+      redirect_uri: import.meta.env.VITE_APP_URL + "/"
+    }
+  }))
   .mount('#app')
