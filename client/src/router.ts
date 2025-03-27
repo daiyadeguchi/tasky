b3e174363ca7ab9a050f4bb0845ee1e877c4d1b3
daiyadeguchi <double.d.rugby@gmail.com>
2025-03-28 07:01:56 +0900

    shut ts up

--- client/src/router.ts
+++ client/src/router.ts
@@ -1,9 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router';
 import Kanban from './components/Kanban.vue';
+import Login from './components/Login.vue'
+import * as VueRouter from 'vue-router';
 
-const routes = [
+const routes: VueRouter.RouteRecordRaw[] = [
   { path: '/', component: Kanban },
-  { path: '/login', component: 'Login.vue' }
+  { path: '/login', component: Login }
 ]
 
 const router = createRouter({
